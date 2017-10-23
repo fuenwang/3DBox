210a1e4d0d483b0868affe9a8309b461b2800715
fuenwang <futon84717@gmail.com>
2017-10-23 23:15:25 +0800

    gg

---
+++ Test/Data.py
@@ -0,0 +1,109 @@
+import os
+import cv2
+import numpy as np
+from collections import OrderedDict
+
+ROOT = './data'
+LABEL_PATH = '%s/labels'%ROOT
+FRAME_PATH = '%s/frames'%ROOT
+
+def ReadLabel(ID):
+    with open('%s/%s.txt'%(LABEL_PATH, ID), 'r') as f:
+        data = OrderedDict()
+        for line in f:
+            line = line[:-1].split('\t')
+            frame_id = line[0]
+            track_id = line[1]
+            top_x = int(line[3])
+            top_y = int(line[4])
+            bottom_x = int(line[5])
+            bottom_y = int(line[6])
+
+            top = (top_x, top_y)
+            bottom = (bottom_x, bottom_y)
+
+            if track_id not in data:
+                data[track_id] = OrderedDict()
+            data[track_id][frame_id] = OrderedDict((('top', top), ('bottom', bottom)))
+    return data
+
+class Data:
+    def __init__(self, ID):
+        self.labels = ReadLabel(ID)
+        frame_path = '%s/%s'%(FRAME_PATH, ID)
+        img_lst = [x for x in sorted(os.listdir(frame_path))]
+        self.img = OrderedDict()
+        for img_name in img_lst:
+            img_id = img_name.split('.')[0]
+            img_path = '%s/%s'%(frame_path, img_name)
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            self.img[img_id] = img.astype(np.float32) / 255
+        self.track_lst = sorted(self.labels.keys())
+        self.frame_lst = sorted(self.img.keys())
+
+        self.result = OrderedDict()
+        for one in self.track_lst:
+            self.result[one] = OrderedDict()
+
+        self.now_track = 0
+        self.now_frame = 0
+
+    def Next(self):
+        track_id = self.track_lst[self.now_track]
+        frame_id = self.labels[track_id].keys()[self.now_frame]
+        update_info = (track_id, frame_id) 
+        top = self.labels[track_id][frame_id]['top']
+        bottom = self.labels[track_id][frame_id]['bottom']
+        img = self.img[frame_id]
+
+        crop = img[top[1]:bottom[1]+1, top[0]:bottom[0]+1, :]
+
+        if self.now_frame == len(self.labels[track_id].keys()) - 1:
+            self.now_frame = 0
+            self.now_track += 1
+        else:
+            self.now_frame += 1
+
+        if self.now_track == len(self.track_lst):
+            stop = True
+        else:
+            stop = False
+
+        crop_resize = cv2.resize(crop, (224, 224), cv2.INTER_CUBIC)
+        batch = np.zeros([1, 3, 224, 224], np.float64)
+        batch[:, 0, :, :] = crop_resize[:, :, 2]
+        batch[:, 1, :, :] = crop_resize[:, :, 1]
+        batch[:, 2, :, :] = crop_resize[:, :, 0]
+        return batch, stop, crop, update_info
+    
+    def Update(self, info, orientation, dimension):
+        track_id = info[0]
+        frame_id = info[1]
+
+        self.result[track_id][frame_id] = {'orientation':orientation, 'dimension':dimension}
+
+if __name__ == '__main__':
+    data = Data('000001')
+
+    stop = False
+    #data.now_track = 3
+    #data.now_frame = 9
+    while not stop:
+        #print (data.now_track, data.now_frame)
+        [img, stop, crop, info] = data.Next()
+        cv2.namedWindow('GG')
+        cv2.imshow('GG', crop)
+        #cv2.imshow('GG', big)
+        cv2.waitKey(0)
+        #exit()
+
+
+
+
+
+
+
+
+
+
+
---
+++ Test/test.py
@@ -0,0 +1,66 @@
+import os
+import sys
+sys.path.append('..')
+import cv2
+import torch
+import Dataset
+import Data
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import vgg
+from Model import Model
+from torch.autograd import Variable
+
+
+def Batch2Image(batch):
+    img = np.zeros([224, 224, 3], np.float32)
+    try:
+        batch = batch.cpu().data.numpy()
+    except:
+        pass
+
+    img[:, :, 0] = batch[0, 2, :, :]
+    img[:, :, 1] = batch[0, 1, :, :]
+    img[:, :, 2] = batch[0, 0, :, :]
+    return img
+
+if __name__ == '__main__':
+    bins = 2
+    w = 1
+    alpha = 1
+    dataset = Dataset.ImageDataset('../../Kitti/training')
+    dataset = Dataset.BatchDataset(dataset, 1, bins, mode='eval')
+    #print 'a'    
+    param = torch.load('model.pkl')
+    VGG = vgg.vgg19_bn(pretrained=False)
+    model = Model(features=VGG.features, bins=bins).cuda()
+    model.load_state_dict(param)
+    model.eval()
+
+    ID = '000012'
+    data = Data.Data(ID)
+
+    stop = False
+    while not stop:
+        [batch, stop, crop, info] = data.Next()
+        batch_v = Variable(torch.FloatTensor(batch), requires_grad = False).cuda()
+
+        [orient, confi, dim] = model(batch_v)
+        orient = orient.cpu().data.numpy()[0, :, :]
+        conf = confi.cpu().data.numpy()[0, :]
+        dim = dim.cpu().data.numpy()[0, :]
+        argmax = np.argmax(conf)
+        orient = orient[argmax, :]
+        cos = orient[0]
+        sin = orient[1]
+        theta = np.arctan2(sin, cos) / np.pi * 180
+        theta =  theta + dataset.centerAngle[argmax] / np.pi * 180 
+        if theta < 0:
+            theta += 360
+        print theta
+        print dim
+        cv2.namedWindow('GG')
+        cv2.imshow('GG', crop)
+        cv2.waitKey(0)
+
